882348d7235cd81b9e84f738cae0919f32b6dece
Rafael Souza <rafael.movility@gmail.com>
2017-09-22 15:07:08 -0300

    Telas-Medei v0.1

---
+++ js/ValidacaoForm.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("input").blur(function(){
+     if($(this).val() == "")
+         {
+             $(this).css({"border-color" : "#F00", "padding": "2px"});
+         }
+    });
+})
+
